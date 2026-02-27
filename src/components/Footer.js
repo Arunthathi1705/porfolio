76165117d6ad6de54275502ab8ef46f64aa63b63
Arunthathi1705 <arunthathigurumoorthi@gmail.com>
2026-02-27 17:01:25 +0530

    Updated layout, styles, and assets

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import "./../styles/Footer.css"
+import "./../styles/Footer.css";
+import linkedin from '../components/Assets/linkedin.png';
+import github from '../components/Assets/github.png';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      &#169; 2025 Akalya. All Rights Reserved
+      <p> &#169; 2025 Akalya. All Rights Reserved</p>
+      <div className='social-icons'>
+        <a href="https://www.linkedin.com/in/akalya-gurumoorthi-324a452a4/" target="_blank" rel="noopener noreferrer">
+          <img src={linkedin} alt="linkedin" height="30px" width="30px" />
+        </a>
+        <a href="https://github.com/Akalya2403" target="_blank" rel="noopener noreferrer">
+          <img src={github} alt="github" height="30px" width="30px" />
+        </a>
+      </div>
+
     </footer>
   )
 }
